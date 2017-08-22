8effbf62dc3fea091122ad9e87ad65cc47460623
Benjamin <benjamin-lopez@hotmail.fr>
2017-08-23 01:00:36 +0200

    Add validation and max attempts to the code generator CasperJS script.

--- generator.js
+++ generator.js
@@ -1,3 +1,8 @@
+// Setup
+var MAX_ATTEMPTS = 50;
+var attempts = 0;
+
+// Utils
 function pad(num, size) {
     var it = String(num);
     while (it.length < size) it = '0' + it;
@@ -6,13 +11,13 @@ function pad(num, size) {
 
 var casper = require('casper').create();
 
+casper.start('https://www.bkvousecoute.fr');
+
 casper.then(function() {
     this.click('input[type=submit]');
 });
 
 casper.then(function() {
-    this.sendKeys('#SurveyCode', '22365');
-
     var date = new Date();
     var day = date.getUTCDate();
     var month = date.getUTCMonth() + 1; //months from 1-12
@@ -31,6 +36,9 @@ casper.then(function() {
 });
 
 casper.continueUntilCode = function() {
+    attempts++;
+    if(attempts >= MAX_ATTEMPTS) this.die('Too many attempts.', 1);
+
     this.then(function() {
         if(this.getTitle() === 'Mon expérience BK - Merci') {
             this.echo(this.fetchText('.ValCode').split(':').reverse()[0].trim());
