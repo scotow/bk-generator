d0ba18fefc014e6d9429ef97b4b9b81f9be12937
Benjamin <benjamin-lopez@hotmail.fr>
2017-08-22 13:25:31 +0200

    Allow only one survey at the same time.

--- app.js
+++ app.js
@@ -6,20 +6,34 @@ app.use(express.static(__dirname + '/public'));
 
 const PORT = 4002;
 
+var surveying = false;
+
 app.post('/code', function(req, res) {
-    shell.exec('node /root/casperjs/casperjs/bin/casperjs.js /root/casperjs/casperjs/bk-code-generator.js', {silent: true}, function(code, stdout, stderr) {
-        if(code && code !== 0) {
-            res.status(404).send('Not found');
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(
-                {
-                    status: 'success',
-                    code: stdout.trim()
-                }
-            ));
-        }
-    });
+    if(surveying) {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(
+            {
+                status: 'waiting'
+            }
+        ));
+    } else {
+        surveying = true;
+        shell.exec('node /root/casperjs/casperjs/bin/casperjs.js /root/casperjs/casperjs/bk-code-generator.js', {silent: true}, function(code, stdout, stderr) {
+            if(code && code !== 0) {
+                res.status(404).send('Not found');
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify(
+                    {
+                        status: 'success',
+                        code: stdout.trim()
+                    }
+                ));
+                surveying = false;
+            }
+        });
+    }
+
 });
 
 app.listen(PORT, function () {
--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,29 @@ $(function() {
         $('.loader > .label').text('An error has occurred. Sorry.');
     }
 
-    $.ajax(
-        {
-            url: '/code',
-            method: 'POST',
-            success: function(data) {
-                if(data.status === 'success') {
-                    $('.loader > .label').addClass('done').text(data.code);
-                } else {
-                    onError();
-                }
-            },
-            error: onError
-        }
-    );
+    function getCode() {
+        $.ajax(
+            {
+                url: '/code',
+                method: 'POST',
+                success: function(data) {
+                    switch(data.status) {
+                        case 'success':
+                            $('.loader > .label').addClass('done').text(data.code);
+                            break;
+                        case 'waiting':
+                            $('.loader > .label').text('A Burger is already generating. Please wait a bit.');
+                            setTimeout(getCode, 3 * 1e3);
+                            break;
+                        default:
+                            onError();
+                            break;
+                    }
+                },
+                error: onError
+            }
+        );
+    }
+
+    getCode();
 });
